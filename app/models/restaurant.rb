98937afd1bd4506546a01a982ba948f0031e3cad
Luka Domitrovic <luka@domitrovic.si>
2015-03-25 11:34:02 +0100

    add price_in_eur

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,4 +5,8 @@ class Restaurant < ActiveRecord::Base
 	attr_accessor :full_address
 	geocoded_by :full_address
 	before_save :geocode, if: ->(obj){ obj.address.present? and obj.address_changed? }
+
+	def price_in_eur
+		self.price/100.00 unless price.nil?
+	end
 end
