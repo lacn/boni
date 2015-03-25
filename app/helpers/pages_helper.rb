2210820d92b037a133d1d6f12f001531cc33a21a
Luka Domitrovic <luka@domitrovic.si>
2015-03-25 15:15:43 +0100

    merge fix

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,25 +1,16 @@
 module PagesHelper
   def latitude
-<<<<<<< HEAD
     latitude = get_address["latitude"]
-=======
-    latitude = Geocoder.search(user_ip.to_s).first.latitude.to_f
->>>>>>> origin/master
     latitude = 46.05 if latitude == 0.0
   end
 
   def longitude
-<<<<<<< HEAD
     longitude = get_address["longitude"]
     longitude = 14.5 if longitude == 0.0
   end
 
   def user_ip
     request.env["HTTP_X_FORWARDED_FOR"].try(:split, ',').try(:first) || request.env["REMOTE_ADDR"]
-=======
-    longitude = Geocoder.search(user_ip.to_s).first.longitude.to_f
-    longitude = 14.5 if longitude == 0.0
->>>>>>> origin/master
   end
 
   def get_address
