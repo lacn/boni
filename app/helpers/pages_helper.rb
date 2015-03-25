fc900ad0cf7fdd2674749551a074b24de3f94a59
Luka Domitrovic <luka@domitrovic.si>
2015-03-25 15:12:33 +0100

    implement freegeqip

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,15 +1,19 @@
 module PagesHelper
   def latitude
-    latitude = Geocoder.search(user_ip.to_s).first.latitude.to_f
+    latitude = get_address["latitude"]
     latitude = 46.05 if latitude == 0.0
   end
 
   def longitude
-    longitude = Geocoder.search(user_ip.to_s).first.longitude.to_f
+    longitude = get_address["longitude"]
     longitude = 14.5 if longitude == 0.0
   end
 
   def user_ip
     request.env["HTTP_X_FORWARDED_FOR"].try(:split, ',').try(:first) || request.env["REMOTE_ADDR"]
+  end
+
+  def get_address
+    HTTParty.get('http://freegeoip.net/json/'+user_ip)
   end
 end
