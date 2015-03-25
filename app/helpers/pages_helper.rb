8f636d101221315f680ab9422af7fadac400853d
Luka Domitrovic <luka@domitrovic.si>
2015-03-25 16:05:43 +0100

    back to geocoder

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,19 +1,15 @@
 module PagesHelper
   def latitude
-    latitude = get_address["latitude"]
+    latitude = Geocoder.search(user_ip.to_s).first.latitude
     latitude = 46.05 if latitude == 0.0
   end
 
   def longitude
-    longitude = get_address["longitude"]
+    longitude = Geocoder.search(user_ip.to_s).first.longitude
     longitude = 14.5 if longitude == 0.0
   end
 
   def user_ip
     request.env["HTTP_X_FORWARDED_FOR"].try(:split, ',').try(:first) || request.env["REMOTE_ADDR"]
-  end
-
-  def get_address
-    HTTParty.get('http://freegeoip.net/json/'+user_ip)
   end
 end
