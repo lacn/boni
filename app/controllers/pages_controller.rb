bb13b7e10849f57515daf7a0ef2d014056a44f09
Luka Domitrovic <luka@domitrovic.si>
2015-03-25 11:33:49 +0100

    add infowindow

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,10 @@ class PagesController < ApplicationController
   def home
   	@restaurants = Restaurant.all
   	@hash = Gmaps4rails.build_markers(@restaurants) do |restaurant, marker|
-		  marker.lat restaurant.latitude
-		  marker.lng restaurant.longitude
+		  marker.lat  restaurant.latitude
+		  marker.lng  restaurant.longitude
+      marker.infowindow render_to_string(:partial => "/pages/infowindow",
+                                         :locals => { :restaurant => restaurant})
 		end
   end
 end
