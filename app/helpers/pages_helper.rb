55ce538819d075668526d5554dfc939aaab22eac
Luka Domitrovic <luka@domitrovic.si>
2015-03-25 15:05:48 +0100

    chenge Geocoder for freegeoip

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,12 +1,12 @@
 module PagesHelper
   def latitude
     latitude = Geocoder.search(user_ip.to_s).first.latitude.to_f
-    latitude = 46.0500 if latitude == 0.0
+    latitude = 46.05 if latitude == 0.0
   end
 
   def longitude
     longitude = Geocoder.search(user_ip.to_s).first.longitude.to_f
-    longitude = 14.5000 if longitude == 0.0
+    longitude = 14.5 if longitude == 0.0
   end
 
   def user_ip
