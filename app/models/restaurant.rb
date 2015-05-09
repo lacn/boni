7184c154cf98f4c655afbc87cf43e2f82e43606e
Luka Domitrovic <luka@domitrovic.si>
2015-05-09 13:37:14 +0200

    fix error for exception notifier

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -6,7 +6,7 @@ class Restaurant < ActiveRecord::Base
 	geocoded_by :full_address
 	before_save :geocode, if: ->(obj){ obj.address.present? and obj.address_changed? }
 
-	# def price_in_eur
-	# 	self.price/100.00 unless price.nil?
-	# end
+	def price_in_eur
+		self.price/100.00 unless price.nil?
+	end
 end
