b29753b37efa92b17a8ace565b96e9c3ba8e9cda
Luka Domitrovic <luka@domitrovic.si>
2015-05-10 17:39:18 +0200

    delete old way of showing view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,6 @@
 class PagesController < ApplicationController
   def home
-    @restaurants = Restaurant.all
-    @hash = Gmaps4rails.build_markers(@restaurants) do |restaurant, marker|
-      marker.lat  restaurant.latitude
-      marker.lng  restaurant.longitude
-      marker.infowindow render_to_string(:partial => "/pages/infowindow",
-                                         :locals => { :restaurant => restaurant})
-    end
+    
   end
 
   def restaurants
