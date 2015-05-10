ff2bd1e7f6a059e1612d3d058a045202f46e6b10
Luka Domitrovic <luka@domitrovic.si>
2015-05-10 19:00:40 +0200

    Revert "delete geo position by user ip"
    This reverts commit b9967f1352e1ebb401f8251277589c62a862701a.

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,3 +1,13 @@
 module PagesHelper
+  def latitude
+    Geocoder.search(user_ip.to_s).first.latitude || 46.05
+  end
 
+  def longitude
+    Geocoder.search(user_ip.to_s).first.longitude || 14.5
+  end
+
+  def user_ip
+    request.env["HTTP_X_FORWARDED_FOR"].try(:split, ',').try(:first) || request.env["REMOTE_ADDR"]
+  end
 end
