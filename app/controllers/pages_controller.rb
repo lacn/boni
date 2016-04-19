6d073abb7039b45aecc3ae5ce47ac2441c546de5
Marko Grešak <gresak.marko@gmail.com>
2016-04-19 14:01:05 +0200

    refactor restaurants endpoint to use jbuilder

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,15 +11,7 @@ class PagesController < ApplicationController
   end
 
   def restaurants
-    @restaurants = Restaurant.all
-    @hash = Gmaps4rails.build_markers(@restaurants) do |restaurant, marker|
-      marker.lat  restaurant.latitude
-      marker.lng  restaurant.longitude
-      marker.json({name: restaurant.name, address: restaurant.address, city: restaurant.city.name, price: restaurant.price_in_eur })
-    end
-    respond_to do |format|
-      format.json { render json: @hash }
-    end
+    @restaurants = Restaurant.all.includes(:city)
   end
 
   def version
