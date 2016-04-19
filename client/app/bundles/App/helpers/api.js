ac7bf6b5819760fd2eefd7fd043f8da5ce5cd2ca
Marko Grešak <gresak.marko@gmail.com>
2016-04-19 14:59:06 +0200

    remove console.log statements

--- client/app/bundles/App/helpers/api.js
+++ client/app/bundles/App/helpers/api.js
@@ -11,23 +11,19 @@ import {getValue, storeValue} from './localStorageManager';
 import {restaurantsResponse} from '../actions/AppActionCreators';
 
 export function fetchRestaurants() {
-  console.log('fetch restaurants');
   fetch(RESTAURANTS_ENDPOINT)
     .then(res => res.json())
     .then(restaurants => {
-      console.log(`got ${restaurants.length} restaurants`);
       storeValue(STORAGE_RESTAURANTS_KEY, restaurants);
       return restaurants;
     });
 }
 
 export function fetchVersion() {
-  console.log('fetch version');
   fetch(VERSION_ENDPOINT)
     .then(res => res.json())
     .then(json => json.version)
     .then(version => {
-      console.log('got version', version);
       const currentVersion = getValue(STORAGE_VERSION_KEY).version;
       if (!currentVersion || currentVersion < version) {
         storeValue(STORAGE_VERSION_KEY, version);
